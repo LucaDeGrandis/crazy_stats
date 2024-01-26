2a4853c20e6104f6d4a1527f62a05fb39e6f22d7
LucaDeGrandis <luca.degrandis@altiliagroup.com>
2024-01-26 16:07:37 +0100

    added default to tokenizer_name in nltk_tokenizer

--- text_summarization_stats/tokenizers/nltk_tokenizer.py
+++ text_summarization_stats/tokenizers/nltk_tokenizer.py
@@ -16,10 +16,15 @@ class NLTKTokenizer(Tokenizer):
     def __init__(self, **kwargs) -> None:
         for key, item in kwargs.items():
             setattr(self, key, item)
+        if 'tokenizer_name' in kwargs:
+            assert kwargs['tokenizer_name'] in nltk_tokenizers_map.keys()
+            self.tokenizer_name = kwargs['tokenizer_name']
+        else:
+            self.tokenizer_name = 'word_tokenize'
         super().__init__()
     
-    def create_tokenizer(self, tokenizer_name):
-        self.tokenizer = nltk_tokenizers_map[tokenizer_name]
+    def create_tokenizer(self):
+        self.tokenizer = nltk_tokenizers_map[self.tokenizer_name]
 
     def postprocess_tokenization(
             self,
