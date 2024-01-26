7027e9f94d17f63d4d66c13861acb5d127477a82
LucaDeGrandis <luca.degrandis@altiliagroup.com>
2024-01-26 16:26:02 +0100

    correction to compression

--- text_summarization_stats/compression.py
+++ text_summarization_stats/compression.py
@@ -2,10 +2,10 @@ from typing import List
 
 def compression(
         summary: List[str],
-        reference: List[str],
+        input_document: List[str],
     ) -> float:
     """
     Compression algorithm for strings as defined in https://aclanthology.org/N18-1065.pdf.
     """
 
-    return len(summary) / len(reference)
+    return len(input_document) / len(summary)
