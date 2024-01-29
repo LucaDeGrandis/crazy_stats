2aafef0f97c725ba61b6859be200323fed3f872a
LucaDeGrandis <luca.degrandis@altiliagroup.com>
2024-01-29 13:05:39 +0100

    added sentence_alignment_simple

--- text_summarization_stats/sentence_alignment.py
+++ text_summarization_stats/sentence_alignment.py
@@ -47,6 +47,44 @@ def sentence_alignment_complete(
     return ROUGE_score, alignment
 
 
+def sentence_alignment_simple(
+    target_sentence: str,  # the summary sentences
+    document_sentences: List[str],  # the source sentences
+    rouge_type: str = 'rouge2',  # rhe ROUGE type to use
+) -> Tuple[float, List[str]]:
+    """
+    Performs sentence alignment for a single sentence as defined in the paper https://aclanthology.org/P18-1061.pdf
+    """
+    assert rouge_type in ['rouge1', 'rouge2', 'rougeL', 'rougeLS']
+    assert document_sentences
+    assert isinstance(document_sentences, list)
+    assert isinstance(document_sentences[0], str)
+    assert isinstance(target_sentence, str)
+
+    if len(document_sentences) == 1:
+        return 0.0, document_sentences
+
+    # initialize
+    alignment = []
+    ROUGE_score = 1e-6
+    remaining_sentences = copy.deepcopy(document_sentences)
+    while True:
+        document_sentences_samples = [alignment + [new_sentence] for new_sentence in remaining_sentences]
+        target_sentences = [target_sentence] * (len(document_sentences_samples))
+        ROUGE_scores = rouge.compute(predictions=target_sentences, references=document_sentences_samples, rouge_types=[rouge_type], use_aggregator=False)[rouge_type]
+        best_index = np.argmax(ROUGE_scores)
+        if ROUGE_scores[best_index] > ROUGE_score:
+            ROUGE_score = ROUGE_scores[best_index]
+            alignment += [remaining_sentences.pop(best_index)]
+        else:
+            break
+        if not remaining_sentences:
+            break
+
+    return ROUGE_score, alignment
+
+
+
 def sentence_alignment(
     document_sentences: List[str],  # the source sentences
     summary_sentences: List[str]  # the summary sentences
