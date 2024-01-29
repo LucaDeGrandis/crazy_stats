b04d24d7c89f938b24e557ce53d8bb0cb38509f6
LucaDeGrandis <luca.degrandis@altiliagroup.com>
2024-01-29 13:24:49 +0100

    added Fusion

--- text_summarization_stats/fusion.py
+++ text_summarization_stats/fusion.py
@@ -1,11 +1,31 @@
 from typing import List, Tuple
 
+
 def Fusion(
-    alignment: List[Tuple[str, List[str]]],  # the alignment
+    alignments: List[Tuple[str, List[str]]],  # the alignment
 ) -> float:
     """
-    Computes the fusion score as defined in the paper https://arxiv.org/pdf/2309.04269.pdf
+    Computes the fusion score as defined in the paper https://arxiv.org/pdf/2309.04269.pdf.
+
+    Example usage:
+
+        input_document: str
+        summary: str
+
+        # Tokenization
+        from text_summarization_stats.tokenizers import NLTKTokenizer
+        tokenizer = NLTKTokenizer(**{'tokenizer_name': 'sent_tokenize'})
+        summary_Stokenized = tokenizer(summary)
+        input_document_Stokenized = tokenizer(input_document)
+
+        # Multiple alignments
+        from text_summarization_stats.sentence_alignment import multiple_sentences_alignment
+        multiple_alignment = multiple_sentences_alignment(
+            summary_Stokenized,
+            input_document_Stokenized,
+            **{'alignment_type': 'simple'}
+        )
     """
-    lengths = [len(x[1]) for x in alignment]
+    lengths = list(map(len, alignments))
 
     return sum(lengths) / len(lengths)
