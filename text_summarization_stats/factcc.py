977c37822ce063e0320e229f8d3e9d32cface0dd
LucaDeGrandis <luca.degrandis@altiliagroup.com>
2024-02-08 10:37:26 +0100

    added sentence tokenizer to factcc

--- text_summarization_stats/factcc.py
+++ text_summarization_stats/factcc.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from transformers import BertForSequenceClassification, BertTokenizer
+from .tokenizers.nltk_tokenizer import NLTKTokenizer
 from tqdm import tqdm
 import torch
 
@@ -26,17 +27,20 @@ def compute_FactCC(
     tokenizer = BertTokenizer.from_pretrained(model_name)
     model = BertForSequenceClassification.from_pretrained(model_name)
 
-    # Create the input data
-    input_dict = tokenizer(input_documents, summaries, max_length=512, padding='max_length', truncation='only_first', return_tensors='pt')
-    with torch.no_grad():
-        logits = model(**input_dict).logits
-        preds = logits.argmax(dim=1).tolist()
+    # Use a sentence tokenizer to separate sentences of the system summaries
+    tokenizer = NLTKTokenizer(**{'tokenizer_name': 'sent_tokenize'})
+    new_documents = []
+    new_summaries = []
+    for _doc, _summ in zip(input_documents, summaries):
+        for _sent in tokenizer(_summ):
+            new_documents.append(_doc)
+            new_summaries.append(_sent)
 
     # Run batched evaluation
     predictions = []
-    for i in tqdm(range(0, len(summaries), batch_size)):
-        batch_docs = input_documents[i:min([i+batch_size, len(summaries)])]
-        batch_summs = summaries[i:min([i+batch_size, len(summaries)])]
+    for i in tqdm(range(0, len(new_summaries), batch_size)):
+        batch_docs = new_documents[i:min([i+batch_size, len(new_summaries)])]
+        batch_summs = new_summaries[i:min([i+batch_size, len(new_summaries)])]
         input_dict = tokenizer(batch_docs, batch_summs, max_length=512, padding='max_length', truncation='only_first', return_tensors='pt')
         with torch.no_grad():
             logits = model(**input_dict).logits
